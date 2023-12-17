e2a855fbd4d5374aab2f5045a976844cabb88f76
Andrey Muholzoev <amuk89@mail.ru>
2023-12-17 19:18:07 +0700

    ui comleted

--- KeyerTest/MainWindow.xaml.cs
+++ KeyerTest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,10 +15,50 @@ namespace KeyerTest;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window, IDisposable
 {
+    private Color _color = Color.FromArgb(255,255,255,255);
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnFileImportClicked()
+    {
+        
+    }
+    
+    private void OnFileExportClicked()
+    {
+        
+    }
+    
+    private void OnColorChoseClicked()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+        
+    }
+
+    private void ColorBlock_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textBox = (e.OriginalSource as TextBox);
+        if (textBox == null) return;
+        
+        if (!byte.TryParse(textBox.Text, out var chanel)) return;
+        Console.WriteLine(textBox.Text);
+        
+        if (textBox == RBlock) _color.R = chanel;
+        if (textBox == GBlock) _color.G = chanel;
+        if (textBox == BBlock) _color.B = chanel;
+        
+        Console.WriteLine(_color);
+        
+        if (ColorDisplay == null) return;
+        
+        ColorDisplay.Background = new SolidColorBrush(_color);
     }
 }
